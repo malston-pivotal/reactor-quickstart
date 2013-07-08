d5f1c6be1db4a9a0586c3a2b9bac155e30167c71
Stephane Maldini <smaldini@gopivotal.com>
2013-07-08 19:24:38 +0100

    Fix last() handling

--- composable/src/main/java/reactor/quickstart/StreamTradeServerExample.java
+++ composable/src/main/java/reactor/quickstart/StreamTradeServerExample.java
@@ -18,23 +18,34 @@ import java.util.concurrent.TimeUnit;
  */
 public class StreamTradeServerExample {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		Environment env = new Environment();
 		final TradeServer server = new TradeServer();
 
 		// Rather than handling Trades as events, each Trade is accessible via Stream.
 		Deferred<Trade, Stream<Trade>> trades = Streams.<Trade>defer()
-																	.using(env)
-																	.dispatcher(Environment.RING_BUFFER)
-																	// We can always set a length to a Stream if we know it (completely optional).
-				                          .batch(totalTrades)
-																	.get();
+				.using(env)
+				.dispatcher(Environment.RING_BUFFER)
+						// We can always set a length to a Stream if we know it (completely optional).
+				.batch(totalTrades)
+				.get();
 
 		// We compose an action to turn a Trade into an Order by calling server.execute(Trade).
 		Stream<Order> orders = trades.compose().map(new Function<Trade, Order>() {
 			@Override
 			public Order apply(Trade trade) {
 				return server.execute(trade);
+			}
+		});
+
+		//Consume last order and count down the current latch
+		final CountDownLatch latch = new CountDownLatch(1);
+		Stream<Order> orderLast = orders.last();
+		orderLast.consume(new Consumer<Order>() {
+			@Override
+			public void accept(Order order) {
+				LOG.info("Finished processing");
+				latch.countDown();
 			}
 		});
 
@@ -49,20 +60,13 @@ public class StreamTradeServerExample {
 			trades.accept(trade);
 		}
 
-		// Composables can block until all values have passed through them.
+		// Stream can block until all values have passed through them.
 		// They know when the end has arrived because we set the length earlier.
-		final CountDownLatch latch = new CountDownLatch(1);
-		orders.last().consume(new Consumer<Order>() {
-			@Override
-			public void accept(Order order) {
-				LOG.info("Finished processing");
-				latch.countDown();
-			}
-		});
-		try{
-			latch.await(30,TimeUnit.SECONDS);
-		}catch (InterruptedException e){
-			LOG.error("Failed timeout",e);
+		try {
+			LOG.info("Waiting...");
+			latch.await(15, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			LOG.error("Failed timeout", e);
 		}
 
 		// Stop throughput timer and output metrics.
@@ -76,7 +80,7 @@ public class StreamTradeServerExample {
 		startTime = System.currentTimeMillis();
 	}
 
-	private static void endTimer() throws InterruptedException {
+	private static void endTimer() {
 		endTime = System.currentTimeMillis();
 		elapsed = (endTime - startTime) * 1.0;
 		throughput = totalTrades / (elapsed / 1000);
